4be8b9f5582cd164481b737c209ca88e02d70ab1
Thomas Boskamp <thomas.mboskamp@gmail.com>
2021-05-10 09:24:38 +0200

    completed work on tic-tac-toe project for TOP

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//create gameboard array object with functions
+//to get/set data and reset the array
 const gameBoard = (() => {
     let board = new Array(9);
     const getIndexData = (num) => board[num];
@@ -15,19 +17,20 @@ const gameBoard = (() => {
         
     };
 })();
-
+//player object that returns the marker (X/O) the player is using
 const Player = (marker) => {
     const getMarker = () => marker;
     return {getMarker}
 }
-
+//gamedisplay object to control the flow of the game
 const gameDisplay = (() => {
-
+    //populate player objects
     const playerX = Player("X");
     const playerO = Player("O");
     let count = 0;
     let gameOver = false;
-
+    //get elements from HTML file and give them data-indices corresponding
+    //to the array indices in gameBoard object
     const gridItems = document.querySelectorAll('.grid-item');
     for(i = 0; i < gridItems.length; i++) {
         gridItems[i].setAttribute("data-index", i);
@@ -37,29 +40,31 @@ const gameDisplay = (() => {
     const updateGameText = (text) => {
         gameText.textContent = text;
     }
-
+    const updateGameGrid = (i, gameText) => {
+        gridItems[i].textContent = gameText
+    }
+    //core gameplay function to set gameboard array and update the DOM
     const gamePlay = function(item){
         if(gameBoard.getIndexData(item.dataset.index) === undefined){
             if(count % 2 === 0) {
                 gameBoard.setIndexData(item.dataset.index, playerX.getMarker());
-                item.textContent = playerX.getMarker();
+                updateGameGrid(item.dataset.index, playerX.getMarker());
                 updateGameText("Player O's turn");
             }
             else {
                 gameBoard.setIndexData(item.dataset.index, playerO.getMarker());
-                item.textContent = playerO.getMarker();
+                updateGameGrid(item.dataset.index, playerO.getMarker());
                 updateGameText("Player X's turn");
             }
             count++;
         }
         winGame();
-        
     }
     gridItems.forEach(item => item.addEventListener('click', (e) => {
         if(gameOver === true)return;
         gamePlay(e.target);
     }))
-   
+   //reset gameboard array and the DOM
     const resetGrid = () => {
         gridItems.forEach(item => item.textContent = "")
         gameBoard.resetBoard();
@@ -70,7 +75,7 @@ const gameDisplay = (() => {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', () => resetGrid());
     
-   
+   //following functions to check various win conditions/tie condition
     const checkRows = () => {
         let i = 0;
         while (i < 7){
@@ -129,16 +134,9 @@ const gameDisplay = (() => {
         playerO,
         winGame,
         gameOver,
-        
+        gamePlay,
         
     };
 
 })();
 
-const gameComputer = (() => {
-    
-
-    return {
-
-    }
-})();
